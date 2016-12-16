10f26b423f0cc91488fd7e4c89113fb1f745b09d
Onofre Martorell <>
2016-12-16 13:32:13 +0100

    Pruebas pca

--- Code/Session1/session1.py
+++ Code/Session1/session1.py
@@ -20,7 +20,7 @@ D,L=mySIFTDescriptor.extractFeatures(train_images_filenames,train_labels,max_cla
 
 #Apply PCA to descriptors
 print 'Applying PCA'
-number_components = 100
+number_components = 110
 D, pca = PCA_computing.PCA_to_data(D, number_components)
 
 # Train a linear SVM classifier
@@ -43,4 +43,13 @@ accuracy=Evaluation.computeAccuracy(predictedClasses,test_labels)
 end=time.time()
 print 'Done in '+str(end-start)+' secs.'
 
-## 38.78% in 797 secs.
+## PCA:128 38.78% in 797 secs.
+## PCA:120 39.52% in 885 secs.
+## PCA:110 38.66% in 996 secs.
+## PCA:100 39.03% in 646 secs.
+## PCA:90 40.64% in 708 secs.
+## PCA:80 39.15% in 636 secs.
+## PCA:70 39.65% in 821 secs.
+
+
+
