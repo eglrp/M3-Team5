08f422f26f4e733efcf76a2f651821e666abd882
Idoia Ruiz <idoiaruizl@gmail.com>
2016-12-18 18:15:33 +0100

    arreglo conflictos

--- Code/Session1/session1.py
+++ Code/Session1/session1.py
@@ -1,76 +1,60 @@
+#!/bin/env python
+import sys
+sys.path.append('.')
 import time
 import inputOutputUtils
 import Evaluation
 import PCA_computing
-from descriptors import SIFTDescriptor
-from descriptors import SURFDescriptor
-from descriptors import ORBDescriptor
+from descriptors import SIFTDescriptor,SURFDescriptor,ORBDescriptor
 from SVMClassifiers import SVMClassifier
-from sklearn.externals import joblib
+def launchsession1():
+    start = time.time()
+    
+    # Read the train and test files
+    train_images_filenames,test_images_filenames,train_labels,test_labels=inputOutputUtils.readData()
+    
+    # Create Descriptors (SIFT, SURF, etc)
+    mySIFTDescriptor=SIFTDescriptor();#SIFT Descriptor
+    #mySURFDescriptor = SURFDescriptor(); # SURF Descriptor
+    #myORBDescriptor = ORBDescriptor(); # ORB Descriptor
 
-start = time.time()
+    # Obtain descriptors and labels for the training set
+    max_class_train_images=30
+    D,L=mySIFTDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
+    #D,L=mySURFDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
+    #D,L=myORBDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
+    
+    #Apply PCA to descriptors
+    print 'Applying PCA'
+    number_components = 90
+    D, pca = PCA_computing.PCA_to_data(D, number_components)
+    
+    # Train a SVM classifier
+    mySVMClassifier=SVMClassifier(C=1,kernel_type='linear')#SVMClassifier with C=1 and linear Kernel
+    #mySVMClassifier=SVMClassifier(C=1,kernel_type='rbf')
+    #mySVMClassifier=SVMClassifier(C=1,kernel_type='poly',degree_value = 1, gamma_value = 0.01,weight = 'balanced')
 
-# Read the train and test files
-train_images_filenames,test_images_filenames,train_labels,test_labels=inputOutputUtils.readData()
+    clf, stdSlr=mySVMClassifier.train(D,L)
 
-# Create Descriptors (SIFT, SURF, etc)
-mySIFTDescriptor=SIFTDescriptor();# SIFT Descriptor
-#mySURFDescriptor = SURFDescriptor(); # SURF Descriptor
-#myORBDescriptor = ORBDescriptor(); # ORB Descriptor
+    
+    # Get all the test data and predict their labels
+    predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,mySIFTDescriptor,clf,stdSlr, pca)
+    #predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,mySURFDescriptor,clf,stdSlr, pca)
+    #predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,myORBDescriptor,clf,stdSlr, pca)
+    
+    
+    # Performance evaluation
+    accuracy=Evaluation.computeAccuracy(predictedClasses,test_labels)
 
-
-# Obtain descriptors and labels for the training set
-max_class_train_images=30
-max_class_train_images = 70
-
-D,L=mySIFTDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
-#D,L=mySURFDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
-<<<<<<< HEAD
-#D,L=myORBDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
-
-#Apply PCA to descriptors
-print 'Applying PCA'
-number_components = 15
-D, pca = PCA_computing.PCA_to_data(D, number_components)
-
-# Train a linear SVM classifier
-#mySVMClassifier=SVMClassifier(C=1,kernel_type='linear')#SVMClassifier with C=1 and linear Kernel
-#mySVMClassifier=SVMClassifier(C=1,kernel_type='rbf')
-mySVMClassifier=SVMClassifier(C=1,kernel_type='poly')
-=======
-# D,L=myORBDescriptor.extractFeatures(train_images_filenames,train_labels,max_class_train_images)
-
-#Apply PCA to descriptors
-print 'Applying PCA'
-number_components = 90
-
-
-D, pca = PCA_computing.PCA_to_data(D, number_components)
-
-# Train a linear SVM classifier
-mySVMClassifier=SVMClassifier(C=10,kernel_type='rbf',degree_value = 1, gamma_value = 0.01,weight = 'balanced')#SVMClassifier with C=1 and linear Kernel
->>>>>>> 86f1015147d095f1f98d1896bb12dcb97b200e3b
-
-
-#TODO: Try other parameters for SVM
-clf, stdSlr=mySVMClassifier.train(D,L)
-
-#Save the trained SVM
-#joblib.dump(clf, 'SVMClassifier.pkl')
-
-# Get all the test data and predict their labels
-predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,mySIFTDescriptor,clf,stdSlr, pca)
-#predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,mySURFDescriptor,clf,stdSlr, pca)
-#predictedClasses=mySVMClassifier.predict(test_images_filenames,test_labels,myORBDescriptor,clf,stdSlr, pca)
-
-
-
-# Performance evaluation
-accuracy=Evaluation.computeAccuracy(predictedClasses,test_labels)
-accuracy = Evaluation.computeAccuracy(predictedClasses,test_labels)
-
-
-end=time.time()
-print 'Done in '+str(end-start)+' secs.'
-
-## 38.78% in 797 secs.
+    end=time.time()
+    print 'Done in '+str(end-start)+' secs.'
+    
+    f = open("output.txt", 'w')
+    f.write(str(accuracy))
+    f.close()
+    
+    ## 38.78% in 797 secs.
+    
+    
+if __name__ == '__main__':
+    launchsession1()
