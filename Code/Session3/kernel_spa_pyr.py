fa3af50b762122b53888bb9146619e61a087262f
Roque <roque.rodriguez.outeiral@gmail.com>
2017-01-14 19:40:47 +0100

    spatial pyramid kernel scheleton

---
+++ Code/Session3/kernel_spa_pyr.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 18:07:33 2017
+
+@author: Roque
+"""
+import numpy as np
+
+
+def spatialPyramidKernel(visual_words1,visual_words2,levels_pyramid,k):
+    k_py = 0
+    #First, level 0 is computed
+    
+
+    return k_py
+    
