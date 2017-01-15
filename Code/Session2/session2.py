7475035489e6df35074f57b1629792005d414db2
Idoia Ruiz <idoiaruizl@gmail.com>
2017-01-15 16:28:05 +0100

    session2 launcher cluster

--- Code/Session2/session2.py
+++ Code/Session2/session2.py
@@ -5,7 +5,7 @@ sys.path.append('.')
 import time
 import descriptors, SVMClassifiers, Evaluation, dataUtils,BoW,graphs
 import matplotlib.pyplot as plt
-def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,useKernelInter,rocCurveCM):
+def launchsession2(num_slots,descriptor_type,randomSplits,levels_pyramid,useKernelInter,rocCurveCM):
     start = time.time()
     
     # Read the train and test files
@@ -19,7 +19,7 @@ def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,us
         TrainingSplit, ValidationSplit=dataUtils.getTrainingValidationSplit(train_images_filenames,train_labels,train_percentage)
     
     #Get descriptors D
-    if Use_spatial_pyramid != 0:
+    if levels_pyramid != 0:
         D, Train_descriptors, Train_label_per_descriptor, Train_keypoints, Train_image_size = descriptors.extractFeaturesPyramid(TrainingSplit,descriptor_type,num_slots)
     else:
         D, Train_descriptors, Train_label_per_descriptor = descriptors.extractFeatures(TrainingSplit, descriptor_type,num_slots)
@@ -29,8 +29,8 @@ def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,us
     codebook=BoW.computeCodebook(D,k)
 
     #Determine visual words
-    if Use_spatial_pyramid != 0:
-        visual_words = BoW.getVisualWordsSpatialPyramid(codebook, k, Train_descriptors, Train_image_size, Train_keypoints, Use_spatial_pyramid)
+    if levels_pyramid != 0:
+        visual_words = BoW.getVisualWordsSpatialPyramid(codebook, k, Train_descriptors, Train_image_size, Train_keypoints, levels_pyramid)
     else:    
         visual_words = BoW.getVisualWords(codebook, k, Train_descriptors)
     
@@ -44,12 +44,12 @@ def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,us
 
     #For test set
     if useKernelInter:
-        predictedLabels2=SVMClassifiers.predictKernelIntersection(test_images_filenames,descriptor_type,clf,stdSlr,train_scaled,k,codebook,Use_spatial_pyramid,num_slots)
+        predictedLabels2=SVMClassifiers.predictKernelIntersection(test_images_filenames,descriptor_type,clf,stdSlr,train_scaled,k,codebook,levels_pyramid,num_slots)
         accuracy2 = Evaluation.computeAccuracyOld(predictedLabels2,test_labels)
         print 'Final Kernel intersection test accuracy: ' + str(accuracy2)
     else:
         # Get all the test data and predict their labels
-        predictedLabels=SVMClassifiers.predict(test_images_filenames,descriptor_type,stdSlr, codebook, k, Use_spatial_pyramid,num_slots)
+        predictedLabels=SVMClassifiers.predict(test_images_filenames,descriptor_type,stdSlr, codebook, k, levels_pyramid,num_slots)
         #Compute accuracy
         accuracy = Evaluation.getMeanAccuracy(clf,predictedLabels,test_labels)
         print 'Final test accuracy: ' + str(accuracy)
@@ -58,12 +58,12 @@ def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,us
     validation_images_filenames,validation_labels=dataUtils.unzipTupleList(ValidationSplit)
     if useKernelInter:
         #Kernel intersection
-        predictedLabels2=SVMClassifiers.predictKernelIntersection(validation_images_filenames,descriptor_type,clf,stdSlr,train_scaled,k,codebook,Use_spatial_pyramid,num_slots)
+        predictedLabels2=SVMClassifiers.predictKernelIntersection(validation_images_filenames,descriptor_type,clf,stdSlr,train_scaled,k,codebook,levels_pyramid,num_slots)
         accuracy2 = Evaluation.computeAccuracyOld(predictedLabels2,validation_labels)
         print 'Final Kernel intersection validation accuracy: ' + str(accuracy2)
     else:
         # Get all the test data and predict their labels
-        predictedLabels=SVMClassifiers.predict(validation_images_filenames,descriptor_type,stdSlr, codebook, k, Use_spatial_pyramid,num_slots)
+        predictedLabels=SVMClassifiers.predict(validation_images_filenames,descriptor_type,stdSlr, codebook, k, levels_pyramid,num_slots)
         #Compute accuracy
         validation_accuracy = Evaluation.getMeanAccuracy(clf,predictedLabels,validation_labels)
         print 'Final validation accuracy: ' + str(validation_accuracy)
@@ -78,12 +78,12 @@ def launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,us
 
 if __name__ == '__main__':
     num_slots=4
-    Use_spatial_pyramid = [[2,2], [4,4]]
     useKernelInter = False
     randomSplits = True
+    levels_pyramid = 0#[[2,2], [4,4]]
     rocCurveCM = False
 
     # "SIFT", "SURF", "ORB", "HARRIS", "DENSE"
-    descriptor_type = "DENSE"
-    print "Using %s detector, randomSplits=%s, Use_spatial_pyramid=%s, useKernelInter=%s" % (descriptor_type,randomSplits,Use_spatial_pyramid,useKernelInter)
-    launchsession2(num_slots,descriptor_type,randomSplits,Use_spatial_pyramid,useKernelInter,rocCurveCM)
+    descriptor_type = "SIFT"
+    print "Using %s detector, randomSplits=%s, levels_pyramid=%s, useKernelInter=%s" % (descriptor_type,randomSplits,levels_pyramid,useKernelInter)
+    launchsession2(num_slots,descriptor_type,randomSplits,levels_pyramid,useKernelInter,rocCurveCM)
---
+++ Code/Session2/session2cluster.py
@@ -0,0 +1,58 @@
+#!/bin/env python
+import sys
+sys.path.append('.')
+
+import session2
+import re
+
+if __name__ == '__main__':
+    levels_pyramid = 0
+    useKernelInter = False
+    randomSplits = True
+    
+    try:
+        if len(sys.argv) < 3:
+            raise ValueError, "Needed number of slots and detector type."
+        num_slots = int(sys.argv[1])
+        descriptor_type = sys.argv[2]
+        index = 3
+        while index < len(sys.argv):
+            if sys.argv[index].lower() == '--levelsp':
+                lps=sys.argv[index + 1]
+                if lps=='0':
+                    levels_pyramid=0
+                else:
+                    p = re.compile('\[(\d.+?\d)\]')
+                    rs=p.findall(lps)
+                    if len(rs)==0:
+                        raise ValueError, "The number of levels must be [[x,x],[y,y]]."
+                    levels_pyramid=[]
+                    for r in rs:
+                        levels=r.split(',')
+                        levels_pyramid.append([int(levels[0]),int(levels[1])])
+                index += 2
+            elif sys.argv[index].lower() == '--usehik':
+                useKernelInter = True
+                index += 1
+            elif sys.argv[index].lower() == '--norandoms':
+                randomSplits = False
+                index += 1
+            else:
+                raise ValueError, "Unknown parameter: %s" % (sys.argv[index])
+    except:
+        print "Unhanded exception while parsing the user parameters:", sys.exc_info()[1]
+        print ""
+        print "Syntax:"
+        print "$ python launchcluster.py <number_of_slots> <descriptor_type> (--levelsp <pyramid_levels> | --usehik | --norandoms)*"
+        print ""
+        print "where:"
+        print "        <number_of_slots>    number of processes used in multiprocessing."
+        print "        <descriptor_type>    descriptor type (DENSE, SIFT, SURF, ORB, HARRIS)."
+        print " --levelsp <pyramid_levels>  number of levels of the spatial pyramid, if used (default=0)."
+        print "           --norandoms       disable random validation splits for cross validation."
+        print "           --usehik          enable to use the histogram intersection kernel."
+        print ""
+        raise
+    
+    print "Using %s detector, randomSplits=%s, levels_pyramid=%s, useKernelInter=%s" % (descriptor_type,randomSplits,levels_pyramid,useKernelInter)
+    session2.launchsession2(num_slots,descriptor_type,randomSplits,levels_pyramid,useKernelInter,False)
