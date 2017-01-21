afa2cde972edae148ceaf3c903da3529f67c6cef
Idoia Ruiz <idoiaruizl@gmail.com>
2017-01-21 19:32:09 +0100

    no multiprocessing version

---
+++ Code/Session4-noMP/descriptors.py
@@ -0,0 +1,87 @@
+
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+
+def getBaseModel():
+    CNN_base_model = VGG16(weights='imagenet')
+    return CNN_base_model
+
+def getDescriptors(x, layer_taken, CNN_base_model, CNN_new_model):
+    #Descriptors depending on the chosen layer
+    if layer_taken == 'fc2' or layer_taken == 'fc1':
+        #Extract features from last layer
+        des=CNN_new_model.predict(x)
+    elif layer_taken == 'block5_pool':
+        des = np.array([0, 0])
+        
+        #TO DO: decide how to get the information from this layer
+    #des must be a numpy array with rows corresponding to different descriptors
+    return des
+
+#Extract features methods
+def extractFeaturesMaps(FLSubset, layer_taken, CNN_base_model):
+    #Crop the model up to a certain layer
+    CNN_new_model = Model(input=CNN_base_model.input, output=CNN_base_model.get_layer(layer_taken).output)
+    
+    #Shared data for pool
+    data = [layer_taken, CNN_base_model, CNN_new_model]
+    
+    deslab=[]
+    initPool(data)
+    for FL in FLSubset:
+        deslab.append(getFeaturesAndLabelsForImage(FL))
+
+    labels = [x[1] for x in deslab]
+    Train_descriptors = [x[0] for x in deslab]
+    #print Train_descriptors[0]
+    #print len(Train_descriptors[0])
+    #print type(Train_descriptors)
+    #print type(Train_descriptors[0])
+    # Transform everything to numpy arrays
+    
+    if layer_taken == 'fc1' or layer_taken == 'fc2' or layer_taken == 'flatten':
+        #Not BoVW
+        Train_label_per_descriptor=labels
+        D=Train_descriptors[0]
+        for i in range(1,len(Train_descriptors)):
+            D=np.vstack((D,Train_descriptors[i]))
+    else:
+        size_descriptors = Train_descriptors[0].shape[1]
+        D = np.zeros((np.sum([len(p) for p in Train_descriptors]), size_descriptors), dtype=np.float32)
+        #print D.shape
+        
+        startingpoint = 0
+        Train_label_per_descriptor = np.array([labels[0]]*Train_descriptors[0].shape[0])
+        D[startingpoint:startingpoint + len(Train_descriptors[0])] = Train_descriptors[0]
+        startingpoint += len(Train_descriptors[0])
+        
+        for i in range(1,len(Train_descriptors)):
+            Train_label_per_descriptor = np.hstack((Train_label_per_descriptor,np.array([labels[i]]*Train_descriptors[i].shape[0])))
+            D[startingpoint:startingpoint + len(Train_descriptors[i])] = Train_descriptors[i]
+            startingpoint += len(Train_descriptors[i])
+    
+    return D, Train_descriptors, Train_label_per_descriptor
+
+def getFeaturesAndLabelsForImage((filename,label)):
+    print 'Reading image ' + filename + ' with label ' + label
+    layer_taken = data[0]
+    CNN_base_model = data[1]
+    CNN_new_model = data[2]
+    
+    img = image.load_img(filename, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis = 0)
+    x = preprocess_input(x)
+
+    des = getDescriptors(x, layer_taken, CNN_base_model, CNN_new_model)
+
+    return (des, label)
+
+#Multiprocessing utils
+def initPool(data_):
+    # data to share with processes
+    global data
+    data = data_
--- Code/Session4-noMP/session4.py
+++ Code/Session4-noMP/session4.py
@@ -5,7 +5,7 @@ sys.path.append('.')
 import time
 import SVMClassifiers, Evaluation, dataUtils,BoW, descriptors
 
-def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
+def launchsession4(layer_taken, randomSplits, k, useServer):
     start = time.time()
     
     # Read the train and test files
@@ -15,10 +15,10 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
         train_images_filenames, test_images_filenames, train_labels, test_labels = dataUtils.readData()
     
     #For testing with smaller database
-    train_images_filenames=train_images_filenames[:300]
-    test_images_filenames=test_images_filenames[:300]
-    train_labels=train_labels[:300]
-    test_labels=test_labels[:300]
+    #train_images_filenames=train_images_filenames[:300]
+    #test_images_filenames=test_images_filenames[:300]
+    #train_labels=train_labels[:300]
+    #test_labels=test_labels[:300]
     
     #Divide training into training and validation splits
     train_percentage = 0.7 #70% training 30%validation
@@ -30,8 +30,7 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
     #Obtain information from VGG ConvNet
     CNN_base_model = descriptors.getBaseModel()#Base model
     #Compute features
-    print 'Extracting features'
-    D, Train_descriptors, Train_label_per_descriptor = descriptors.extractFeaturesMaps(TrainingSplit, layer_taken, CNN_base_model, num_slots)
+    D, Train_descriptors, Train_label_per_descriptor = descriptors.extractFeaturesMaps(TrainingSplit, layer_taken, CNN_base_model)
     
     if layer_taken == 'fc1' or layer_taken == 'fc2' or layer_taken == 'flatten':
         visual_words = D
@@ -41,7 +40,7 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
         codebook=BoW.computeCodebook(D,k)
         #Determine visual words
         visual_words = BoW.getVisualWords(codebook, k, Train_descriptors)
-
+    
     # Train a linear SVM classifier
     clf, stdSlr = SVMClassifiers.trainSVM(visual_words,Train_label_per_descriptor,Cparam=1,kernel_type='linear')
 
@@ -49,12 +48,12 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
     TestSplit=zip(test_images_filenames,test_labels)
     if layer_taken == 'fc1' or layer_taken == 'fc2' or layer_taken == 'flatten':
         ##Not using BoVW
-        predictedLabels=SVMClassifiers.predict(TestSplit, layer_taken, stdSlr, clf, CNN_base_model, num_slots)
+        predictedLabels=SVMClassifiers.predict(TestSplit, layer_taken, stdSlr, clf, CNN_base_model)
         accuracy = Evaluation.computeAccuracyOld(predictedLabels,test_labels)
         print 'Final test accuracy: ' + str(accuracy)
     else:
         #BoVW
-        predictedLabels=SVMClassifiers.predictBoVW(TestSplit, layer_taken, stdSlr, codebook, k, CNN_base_model, num_slots)
+        predictedLabels=SVMClassifiers.predictBoVW(TestSplit, layer_taken, stdSlr, codebook, k, CNN_base_model)
         accuracy = Evaluation.getMeanAccuracy(clf,predictedLabels,test_labels)
         print 'Final test accuracy: ' + str(accuracy)
 
@@ -62,12 +61,12 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
     validation_images_filenames, validation_labels = dataUtils.unzipTupleList(ValidationSplit)
     if layer_taken == 'fc1' or layer_taken == 'fc2' or layer_taken == 'flatten':
         #Not using BoVW
-        predictedLabels=SVMClassifiers.predict(ValidationSplit, layer_taken, stdSlr, clf, CNN_base_model, num_slots)
+        predictedLabels=SVMClassifiers.predict(ValidationSplit, layer_taken, stdSlr, clf, CNN_base_model)
         validation_accuracy = Evaluation.computeAccuracyOld(predictedLabels,validation_labels)
         print 'Final validation accuracy: ' + str(validation_accuracy)
     else:
         #BoVW
-        predictedLabels = SVMClassifiers.predictBoVW(ValidationSplit, layer_taken, stdSlr, codebook, k,CNN_base_model, num_slots)
+        predictedLabels = SVMClassifiers.predictBoVW(ValidationSplit, layer_taken, stdSlr, codebook, k,CNN_base_model)
         validation_accuracy = Evaluation.getMeanAccuracy(clf,predictedLabels,validation_labels)
         print 'Final validation accuracy: ' + str(validation_accuracy)
 
@@ -76,13 +75,11 @@ def launchsession4(num_slots, layer_taken, randomSplits, k, useServer):
 
 
 if __name__ == '__main__':
-    num_slots = 4
     randomSplits = False
-    useServer=False
-    method_used = {'layer_taken':'block5_pool', 'method_t_reduce_dim': 'PCA', 'Value_PCA':90}
-    method_used = {'layer_taken':'block5_pool', 'method_t_reduce_dim': 'average', 'Remaining_features':100}
-    layer_taken = "block5_pool"# Layer
+    useServer=True
+
+    layer_taken = "fc2"# Layer
     k = 512 #Centroids for BoVW codebook
 
     print "Taking layer %s , randomSplits = %s, k-means centroids: %s" % (layer_taken, randomSplits, k)
-    launchsession4(num_slots, layer_taken, randomSplits, k, useServer)
+    launchsession4(layer_taken, randomSplits, k, useServer)
